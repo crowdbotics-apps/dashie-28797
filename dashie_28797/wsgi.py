314056b38145115875a8b0780d985ff37bea7f2f
Crowdbotics <team@crowdbotics.com>
2021-07-13 16:50:24 +0000

    Initial commit.

---
+++ dashie_28797/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for dashie_28797 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dashie_28797.settings')
+
+application = get_wsgi_application()
